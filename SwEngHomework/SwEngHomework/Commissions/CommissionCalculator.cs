a2648f494f20e4694660bef8b66ba17f1af7f2ec
xuc@objectcomputing.com <xuc@objectcomputing.com>
2023-06-23 10:59:35 -0400

    Chao Xu

---
+++ SwEngHomework/SwEngHomework/Commissions/CommissionCalculator.cs
@@ -0,0 +1,112 @@
+﻿using Newtonsoft.Json;
+
+namespace SwEngHomework.Commissions
+{
+    public class CommissionCalculator : ICommissionCalculator
+    {
+        public IDictionary<string, double> CalculateCommissionsByAdvisor(string jsonInput)
+        {
+            var report = new Dictionary<string, double>();
+            var data = JsonConvert.DeserializeObject<Data>(jsonInput);
+
+            if(data != null)
+            {
+                var advisors = data.Advisors;
+                var accounts = data.Accounts;
+
+                if(advisors != null && accounts != null)
+                {
+                    foreach(var advisor in advisors)
+                    {
+                        var name = advisor.Name;
+                        report.Add(name, 0.00);
+                        var level = advisor.Level;
+                        var theAccounts = accounts.Where(x => x.Advisor == name);
+                        if(theAccounts != null)
+                        {
+                            var presentValues = theAccounts.Select(x => x.PresentValue);
+                            foreach (var presentValue in presentValues)
+                            {
+                                var commission = GetCommission(level, presentValue);
+                                if (report.ContainsKey(name))
+                                {
+                                    var value = report[name];
+                                    report[name] = value + commission;
+                                }
+                               
+                            }
+                        }
+                                      
+                       
+                    }
+                }
+            }
+
+            return report;
+        }
+
+        private double GetCommission(string level, long presentValue)
+        {
+            double bps;
+
+            if(presentValue < 50000 )
+            {
+                bps = 0.05;
+            }
+            else if (presentValue < 100000)
+            {
+                bps = 0.06;
+            } 
+            else
+            {
+                bps = 0.07;
+            }
+
+            double percent;
+            switch (level)
+            {
+                case "Junior":
+                    percent = 0.25;
+                    break;
+                case "Experienced":
+                    percent = 0.5;
+                    break;
+                case "Senior":
+                    percent = 1.0;
+                    break;
+                default:
+                    percent = 0.0;
+                    break;
+
+            }
+
+            return Math.Round(presentValue * percent * bps/100.0,2);
+        }
+    }
+
+    public class Advisor
+    {
+        public string Name { get; set; }
+        public string Level { get; set; }
+    }
+
+    public enum Level
+    {
+        Senior,
+        Experienced,
+        Junior
+    }
+
+    public class Account
+    {
+        public string Advisor { get; set; }
+        public long PresentValue { get; set; }
+    }
+
+    public class Data
+    {
+        public List<Account> Accounts { get; set; } = new List<Account>();
+        public List<Advisor> Advisors { get; set; } = new List<Advisor>();
+    }
+
+}
---
+++ SwEngHomework/SwEngHomework/DescriptiveStatistics/StatsCalculator.cs
@@ -0,0 +1,83 @@
+﻿namespace SwEngHomework.DescriptiveStatistics
+{
+    public class StatsCalculator : IStatsCalculator
+    {
+        public Stats Calculate(string semicolonDelimitedContributions)
+        {
+           var data = semicolonDelimitedContributions.Split(';');
+           var contributions = new List<double>();
+            foreach (var contributionStr in data) 
+            {
+                var contributionNumber = contributionStr.Trim().TrimStart('$').Replace(",","").Trim();
+
+                if(double.TryParse(contributionNumber, out double contribution))
+                {
+                    contributions.Add(contribution);
+                }
+
+            }
+
+            var stats = new Stats();
+            stats.Median = Median(contributions);
+            stats.Average = Average(contributions);
+            stats.Range = Range(contributions);
+
+            return stats;
+        }
+
+        private double Average(List<double> dataList)
+        {
+            if(dataList.Count == 0)
+            {
+                return 0;
+            }
+            else if (dataList.Count == 1)
+            {
+                return dataList[0];
+            }
+            var ave = dataList.Sum(x => x)/dataList.Count;
+
+            return Math.Round(ave, 2);
+        }
+
+ 
+        private double Median(List<double> dataList)
+        {
+            if (dataList.Count == 0)
+            {
+                return 0;
+            } 
+            else if (dataList.Count == 1)
+            {
+                return dataList[0];
+            }
+            var xs = dataList.ToArray();
+            Array.Sort(xs);
+
+            if(xs.Length % 2== 0)
+            {
+                var media1 = xs[xs.Length / 2 -1];
+                var media2 = xs[xs.Length / 2];
+                return Math.Round((media1 + media2)/2, 2);
+            } else
+            {
+                var media = xs[xs.Length / 2];
+                return Math.Round(media, 2);
+            }
+          
+        }
+
+        private double Range(List<double> dataList)
+        {
+            if (dataList.Count == 0 || dataList.Count == 1)
+            {
+                return 0;
+            }
+
+            var min = dataList.Min();
+            var max = dataList.Max();
+
+            return max - min;
+        }
+    }
+}
